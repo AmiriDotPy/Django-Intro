d6ae8ec724b546de237f6ddf5cde82c18cdad346
AmiriDotPy <amiridotpy@gmail.com>
2024-01-23 23:44:15 +0330

    EDITED ADMIN

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title' , 'created_at' , 'updated_at' , 'publish_at']
+    list_filter = ['created_at']
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name' , 'email' , 'created_at']
+    list_filter = ['email']

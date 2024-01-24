45b1d0bcb5db620d05de15e6602514d3e661b8f5
AmiriDotPy <amiridotpy@gmail.com>
2024-01-24 16:54:20 +0330

    Update admin.py

--- website/admin.py
+++ website/admin.py
@@ -9,7 +9,13 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ['title' , 'created_at' , 'updated_at' , 'publish_at']
     list_filter = ['created_at']
 
+# admin.site.register(Post , PostAdmin)
+    
+
+
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ['name' , 'email' , 'created_at']
     list_filter = ['email']
+
+# admin.site.register(Post , ContactAdmin)

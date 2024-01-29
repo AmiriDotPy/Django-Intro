09cd14203deb98bdf18bc9d1cbe11cf612c8f3cb
AmiriDotPy <amiridotpy@gmail.com>
2024-01-29 20:17:22 +0330

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render , get_object_or_404
 from .models import Post
 from datetime import  datetime
 
+
+def Counter(pid):
+        post = get_object_or_404(Post,id=pid)
+        post.counted_view += 1
+        post.save()
+
+
 def LoadBlogSingle(request , pid):
     post = get_object_or_404(Post,id=pid)
-    post.counted_view += 1
-    post.save()
+    Counter(pid)
     context = {'post' : post}
     return render(request, 'blog/single.html' ,context)
+
+
+   
 
 
 def LoadBlog(request):

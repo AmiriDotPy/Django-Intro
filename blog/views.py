6f232228ba8134695dc89255afa9636f4a2f5764
AmiriDotPy <amiridotpy@gmail.com>
2024-01-31 14:50:01 +0330

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -3,19 +3,16 @@ from .models import Post
 from django.utils import timezone
 
 def Counter(pid):
-        post = get_object_or_404(Post,id=pid)
+        post = get_object_or_404(Post.objects.exclude(publish_at__gt = timezone.now()).exclude(status = 0),id=pid)
         post.counted_view += 1
         post.save()
 
 
 def LoadBlogSingle(request , pid):
     Counter(pid)
-    post = get_object_or_404(Post,id=pid ,status = 1)
+    post = get_object_or_404(Post.objects.exclude(publish_at__gt = timezone.now()).exclude(status = 0),id=pid)
     context = {'post' : post}
     return render(request, 'blog/single.html' ,context)
-
-
-   
 
 
 def LoadBlog(request):

624367dc0d6616d8fbefbfce43a389ba0a6f2a81
AmiriDotPy <amiridotpy@gmail.com>
2024-02-08 14:52:16 +0330

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -6,24 +6,22 @@ from django.utils import timezone
 
 
 def Counter(pid):
-    post = get_object_or_404(Post.objects.exclude(publish_at__gt = timezone.now()),status = 1,id=pid)
+    post = Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , pk = pid)
     post.counted_view += 1
     post.save()
 
 
 def LoadBlogSingle(request , pid):
     Counter(pid)
-    post = get_object_or_404(Post.objects.exclude(publish_at__gt = timezone.now()),status = 1,id=pid)
-    nextPost = get_object_or_404(Post.objects.exclude(publish_at__gt = timezone.now()),status = 1, id__gt = pid)
-    # nextPost = getNextPosts.first()
-    previousPost = get_object_or_404(Post.objects.exclude(publish_at__gt = timezone.now()),status = 1,pk__lt = pid)
-    # previousPost = GetPreviousPosts.first()
+    post = Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , pk = pid)    
+    nextPost =  Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , id__gt = pid).first()
+    previousPost = Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , id__lt = pid).first()
     context = {'post' : post , 'nextpost' : nextPost , 'previouspost' : previousPost}
     return render(request, 'blog/single.html' ,context)
 
 
 def LoadBlog(request):
-    posts = Post.objects.exclude(publish_at__gt = timezone.now() , status = 1)
+    posts = Post.objects.filter(publish_at__lt = timezone.now(), status = 1)
     context = {'posts' : posts}
     return render(request, 'blog/blog.html' , context)
 

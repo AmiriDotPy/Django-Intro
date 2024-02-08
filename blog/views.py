5da3d422363e97401617c71ed6df19627d626863
AmiriDotPy <amiridotpy@gmail.com>
2024-02-08 16:50:21 +0330

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,25 @@
-from django.shortcuts import render , get_object_or_404
-from .models import Post
+from django.shortcuts import render 
 from django.utils import timezone
 
 
 
 
 def Counter(pid):
-    post = Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , pk = pid)
+    post = Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , pk = pid).first()
     post.counted_view += 1
     post.save()
+
+def NextPost(pid):
+    return  Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , id__gt = pid).first()
+def PreviousPost(pid):
+    return Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , id__lt = pid).last()
 
 
 def LoadBlogSingle(request , pid):
     Counter(pid)
-    post = Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , pk = pid)    
-    nextPost =  Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , id__gt = pid).first()
-    previousPost = Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , id__lt = pid).first()
+    post = Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , id = pid).first()
+    nextPost = NextPost(pid)
+    previousPost =PreviousPost(pid)
     context = {'post' : post , 'nextpost' : nextPost , 'previouspost' : previousPost}
     return render(request, 'blog/single.html' ,context)
 

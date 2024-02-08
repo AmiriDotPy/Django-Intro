23bf1aa7f3f32bf86e548e6507e9c224f715e6d5
AmiriDotPy <amiridotpy@gmail.com>
2024-02-08 14:24:20 +0330

    1

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render , get_object_or_404
 from .models import Post
 from django.utils import timezone
-from django.core.paginator import Paginator
 
 
 
 
 def Counter(pid):
-    post = get_object_or_404(Post.objects.exclude(publish_at__gt = timezone.now()).exclude(status = 0),id=pid)
+    post = get_object_or_404(Post.objects.exclude(publish_at__gt = timezone.now()),status = 1,id=pid)
     post.counted_view += 1
     post.save()
 
 
 def LoadBlogSingle(request , pid):
     Counter(pid)
-    post = get_object_or_404(Post.objects.exclude(publish_at__gt = timezone.now()).exclude(status = 0),id=pid)
-    nextPost = get_object_or_404(Post.objects.exclude(publish_at__gt = timezone.now()).exclude(status = 0),id=pid + 1)
-    previousPost = get_object_or_404(Post.objects.exclude(publish_at__gt = timezone.now()).exclude(status = 0),id=pid - 1)
+    post = get_object_or_404(Post.objects.exclude(publish_at__gt = timezone.now()),status = 1,id=pid)
+    nextPost = get_object_or_404(Post.objects.exclude(publish_at__gt = timezone.now()),status = 1, id__gt = pid)
+    # nextPost = getNextPosts.first()
+    previousPost = get_object_or_404(Post.objects.exclude(publish_at__gt = timezone.now()),status = 1,pk__lt = pid)
+    # previousPost = GetPreviousPosts.first()
     context = {'post' : post , 'nextpost' : nextPost , 'previouspost' : previousPost}
     return render(request, 'blog/single.html' ,context)
 
 
 def LoadBlog(request):
-    posts = Post.objects.exclude(publish_at__gt = timezone.now()).exclude(status = 0)
+    posts = Post.objects.exclude(publish_at__gt = timezone.now() , status = 1)
     context = {'posts' : posts}
     return render(request, 'blog/blog.html' , context)
 

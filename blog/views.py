b4ca05713deff97988e40742407c177fd7dea0c0
AmiriDotPy <amiridotpy@gmail.com>
2024-02-10 13:15:54 +0330

    next and previous done

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render 
+from django.shortcuts import render ,get_object_or_404
 from django.utils import timezone
 from .models import Post
 from django.http import Http404
@@ -10,25 +10,17 @@ from django.http import Http404
 
 
 def Counter(pid):
-    post = Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , pk = pid).first()
+    post = get_object_or_404(Post.objects.exclude(publish_at__gt = timezone.now()), status = 1,id=pid)
     post.counted_view += 1
     post.save()
 
-def NextPost(pid):
-    return  Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , id__gt = pid).first()
-def PreviousPost(pid):
-    return Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , id__lt = pid).last()
-
 
 def LoadBlogSingle(request , pid):
-    try:
-        Counter(pid)
-        post = Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , id = pid).first()
-        nextPost = NextPost(pid)
-        previousPost =PreviousPost(pid)
-        context = {'post' : post , 'nextpost' : nextPost , 'previouspost' : previousPost}
-    except Post.DoesNotExist:
-        raise Http404('page not found')
+    Counter(pid)
+    post = get_object_or_404(Post.objects.exclude(publish_at__gt = timezone.now()), status = 1,id=pid)
+    nextPost = Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , id__gt = pid).first()
+    previousPost = Post.objects.filter(publish_at__lt = timezone.now(), status = 1 , id__lt = pid).last()
+    context = {'post' : post ,'nextpost' : nextPost , 'previouspost' : previousPost}
     return render(request, 'blog/single.html' ,context)
 
 
